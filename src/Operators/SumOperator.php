1aedc2f69773c915f5da6d7f1b1f52f9ce26ac2c
Lucas Ferreira <me@ferrl.io>
2018-04-20 15:03:42 -0300

    Write function to pass test

--- src/Operators/SumOperator.php
+++ src/Operators/SumOperator.php
@@ -4,4 +4,15 @@ namespace App\Operators;
 
 class SumOperator
 {
+    /**
+     * Perform sum of two numbers.
+     *
+     * @param int|float|double $a
+     * @param int|float|double $b
+     * @return int|float|double
+     */
+    public function perform($a, $b)
+    {
+        return $a + $b;
+    }
 }
