8b2c282a4cf5e6d94f5876259fb042dc46a909de
Lucas Ferreira <me@ferrl.io>
2018-04-20 16:09:42 -0300

    Write function to pass test

--- src/Calculator.php
+++ src/Calculator.php
@@ -4,4 +4,18 @@ namespace App;
 
 class Calculator
 {
+    /**
+     * @var Interpreter
+     */
+    protected $interpreter;
+
+    /**
+     * Calculator constructor.
+     *
+     * @param Interpreter $interpreter
+     */
+    public function __construct(Interpreter $interpreter)
+    {
+        $this->interpreter = $interpreter;
+    }
 }
