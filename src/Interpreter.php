b219c0bb144004960e3c97ca2858352dd0fa817a
Lucas Ferreira <me@ferrl.io>
2018-04-20 15:22:14 -0300

    Write function to pass test

--- src/Interpreter.php
+++ src/Interpreter.php
@@ -2,6 +2,32 @@
 
 namespace App;
 
+use App\Operators\SumOperator;
+
 class Interpreter
 {
+    /**
+     * Read an expression and return a list of commands to execute.
+     *
+     * @param string $expression
+     * @return array
+     */
+    public function read($expression)
+    {
+        $commands = [];
+
+        preg_match('/(\d+)(?:\s*)([\+\-\*\/])(?:\s*)(\d+)/', $expression, $matches);
+
+        $operator = $matches[2];
+
+        switch ($operator) {
+            case '+':
+                $commands[] = [new SumOperator, [$matches[1], $matches[3]]];
+                break;
+            default:
+                break;
+        }
+
+        return $commands;
+    }
 }
