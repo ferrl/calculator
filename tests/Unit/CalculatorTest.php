bd224629e46f4cba6a9ffc6cf2fde7e92ad9b4b5
Lucas Ferreira <me@ferrl.io>
2018-04-20 16:09:36 -0300

    Failing test

---
+++ tests/Unit/CalculatorTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace tests\Unit;
+
+use App\Calculator;
+use App\Interpreter;
+use App\Operators\Operator;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+
+class CalculatorTest extends TestCase
+{
+    protected $subject;
+
+    public function setUp()
+    {
+        $operator = Mockery::mock(Operator::class);
+        $operator->shouldReceive('perform')->withArgs([1, 3])->andReturn(4);
+
+        $interpreter = Mockery::mock(Interpreter::class);
+        $interpreter->shouldReceive('1 + 3')->andReturn([$operator, [1, 3]]);
+
+        $this->subject = new Calculator($interpreter);
+    }
+
+    public function testExecute()
+    {
+
+        $this->assertTrue(true);
+    }
+}
